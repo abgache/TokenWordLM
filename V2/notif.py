1d0fc9d21c64fc0dd85f7fd945b90b3cc3def755
ABGACHE <143274169+abgache@users.noreply.github.com>
2025-08-13 17:34:31 +0100

    Update notif.py

--- V2/notif.py
+++ V2/notif.py
@@ -1,5 +1,5 @@
-# Send notifications to my phone
 import requests as r
 from import_env import *
 def notif(title, message, image_url="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/449px-Tensorflow_logo.svg.png"):
     pass
+
